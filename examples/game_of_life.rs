772c9a263abbc0b282376cca752963d03d0e2d41
Alice Cecile <alice.i.cecile@gmail.com>
2022-02-19 15:25:27 -0500

    Examples skeleton

---
+++ examples/game_of_life.rs
@@ -0,0 +1,9 @@
+//! An implementation of the Game of Life using `leafwing_2d` types
+//!
+//! Focuses on demonstrating the usage of [`DiscreteCoordinate`] types
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
---
+++ examples/minimal.rs
@@ -0,0 +1,7 @@
+//! A minimal example of how to use `leafwing_2d`
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
