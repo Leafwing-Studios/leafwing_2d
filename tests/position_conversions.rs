7aff2749d3b759165cbfe2648853e3f0a3251893
Alice Cecile <alice.i.cecile@gmail.com>
2022-02-22 19:19:32 -0500

    Tests for position conversion

---
+++ tests/position_conversions.rs
@@ -0,0 +1,73 @@
+use bevy::math::Vec3;
+use leafwing_2d::position::Position;
+
+#[test]
+fn position_to_vec3() {
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(0., 0.)),
+        Vec3::new(0., 0., 0.)
+    );
+
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(1., 0.)),
+        Vec3::new(1., 0., 0.)
+    );
+
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(0., 1.)),
+        Vec3::new(0., 1., 0.)
+    );
+
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(1., 1.)),
+        Vec3::new(1., 1., 0.)
+    );
+
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(-1., -1.)),
+        Vec3::new(-1., -1., 0.)
+    );
+
+    assert_eq!(
+        Vec3::from(Position::<f32>::new(-42., 3.)),
+        Vec3::new(-42., 3., 0.)
+    );
+}
+
+#[test]
+fn vec3_to_position() {
+    assert_eq!(
+        Ok(Position::<f32>::new(0., 0.)),
+        Vec3::new(0., 0., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(1., 0.)),
+        Vec3::new(1., 0., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(0., 1.)),
+        Vec3::new(0., 1., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(1., 1.)),
+        Vec3::new(1., 1., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(-1., -1.)),
+        Vec3::new(-1., -1., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(-42., 3.)),
+        Vec3::new(-42., 3., 0.).try_into()
+    );
+
+    assert_eq!(
+        Ok(Position::<f32>::new(-42., 3.)),
+        Vec3::new(-42., 3., 17.).try_into()
+    );
+}
